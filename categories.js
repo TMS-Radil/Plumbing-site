ee6aa3d5bf89c10915e24b18339ec686eeaa3c1e
TMS-Radil <84691460+TMS-Radil@users.noreply.github.com>
2025-07-08 23:58:16 -0400

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,44 @@
+// categories.js
+const categories = {
+  toilet: {
+    "Pro 45 Fill Valve": 22,
+    "2\" Universal Flapper": 20,
+    "12\" Supply Hose": 11,
+    "16\" Supply Hose": 12,
+    "Wax Ring": 9,
+    "Kohler Flush Seal": 18,
+    "Trip Lever": 25
+  },
+  cpvc_1_2: {
+    "10 Ft 1/2\" CPVC": 12,
+    "1/2\" CPVC Ball Valve": 8,
+    "1/2\" CPVC 90": 1.5,
+    "1/2\" CPVC 90 ST": 2,
+    "1/2\" CPVC Coupling": 1.2,
+    "1/2\" CPVC 45": 1.8,
+    "1/2\" CPVC T": 2.5
+  },
+  cpvc_3_4: {
+    "10 Ft 3/4\" CPVC": 18,
+    "3/4\" CPVC Ball Valve": 12,
+    "3/4\" CPVC 90": 2,
+    "3/4\" CPVC 90 ST": 2.5,
+    "3/4\" CPVC Coupling": 2,
+    "3/4\" CPVC 45": 2.5,
+    "3/4\" CPVC T": 5
+  },
+  valves: {
+    "Angle Stop": 20,
+    "1/4 Turn 1/2\" Boiler Drain": 25,
+    "3/4 Sharkbite Coupling": 13
+  },
+  dwv: {
+    "1 1/2\" DWV 1/8 Bend": 4.5,
+    "1 1/2\" DWV 1/4 Bend": 5,
+    "1 1/2\" DWV 45": 4,
+    "2\" DWV 1/8 Bend": 6,
+    "2\" DWV 1/4 Bend": 6.5,
+    "2\" DWV 45": 5.5,
+    "DWV Cleanout Plug": 3.5
+  }
+};
